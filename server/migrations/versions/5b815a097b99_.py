158de74f96dd7f5ffe9c2c833bb1a1a30f53ab8b
lorna gathumbi <nyawiralornah@gmail.com>
2025-02-02 13:20:29 +0300

    putting it all together lab

---
+++ server/migrations/versions/5b815a097b99_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 5b815a097b99
+Revises: 
+Create Date: 2025-02-02 13:06:55.906195
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '5b815a097b99'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
